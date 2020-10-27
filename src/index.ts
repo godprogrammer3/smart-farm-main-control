e912598ce1ea0c5607f03106366d850d23bbd0e0
godprogrammer <game47you@gmail.com>
2020-10-27 11:06:39 +0700

    Add started service message

--- src/index.ts
+++ src/index.ts
@@ -2,4 +2,5 @@ import "reflect-metadata";
 import NodeManager from "./NodeManager";
 import {container} from "tsyringe";
 const nodeManager: NodeManager = container.resolve(NodeManager);
+console.log("######################## Started Service #####################");
 nodeManager.run();
